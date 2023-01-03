7417df2569e3908dbc60ea7f92acaf80cec28d08
julianomachadoo <julmachadoramos@gmail.com>
2023-01-03 16:22:33 -0300

    Classe indicação e construtor de aluno

---
+++ src/main/java/com/github/julianomachadoo/escola/Indicacao.java
@@ -0,0 +1,12 @@
+package com.github.julianomachadoo.escola;
+
+public class Indicacao {
+
+    private Aluno indicado;
+    private Aluno indicante;
+
+    public Indicacao(Aluno indicado, Aluno indicante) {
+        this.indicado = indicado;
+        this.indicante = indicante;
+    }
+}
