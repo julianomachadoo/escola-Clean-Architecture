03591312bf08d7243a146e7a5bfe6de08280b670
julianomachadoo <julmachadoramos@gmail.com>
2023-01-05 12:03:39 -0300

    RepositorioDeAlunos

---
+++ src/main/java/com/github/julianomachadoo/escola/dominio/aluno/RepositorioDeAlunos.java
@@ -0,0 +1,12 @@
+package com.github.julianomachadoo.escola.dominio.aluno;
+
+import java.util.List;
+
+public interface RepositorioDeAlunos {
+
+    void matricular(Aluno aluno);
+
+    Aluno buscarPorCpf(CPF cpf);
+
+    List<Aluno> listarTodosAlunosMatriculados();
+}
