a56edbaab855c6a82e15219cf774f7f1d470f355
julianomachadoo <julmachadoramos@gmail.com>
2023-01-05 12:49:50 -0300

    RepositorioDeAlunosEmMemoria

---
+++ src/main/java/com/github/julianomachadoo/escola/dominio/infra/aluno/RepositorioDeAlunosEmMemoria.java
@@ -0,0 +1,31 @@
+package com.github.julianomachadoo.escola.dominio.infra.aluno;
+
+import com.github.julianomachadoo.escola.dominio.aluno.Aluno;
+import com.github.julianomachadoo.escola.dominio.aluno.AlunoNaoEncontrado;
+import com.github.julianomachadoo.escola.dominio.aluno.CPF;
+import com.github.julianomachadoo.escola.dominio.aluno.RepositorioDeAlunos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositorioDeAlunosEmMemoria implements RepositorioDeAlunos {
+
+    private List<Aluno> matriculados = new ArrayList<>();
+
+    @Override
+    public void matricular(Aluno aluno) {
+        this.matriculados.add(aluno);
+    }
+
+    @Override
+    public Aluno buscarPorCpf(CPF cpf) {
+        return matriculados.stream().filter(aluno -> aluno.getCpf().equals(cpf.getNumero()))
+                .findFirst()
+                .orElseThrow(() -> new AlunoNaoEncontrado(cpf));
+    }
+
+    @Override
+    public List<Aluno> listarTodosAlunosMatriculados() {
+        return this.matriculados;
+    }
+}
